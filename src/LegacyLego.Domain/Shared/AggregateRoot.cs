f14bd3405abc6c3c2319beef25904a6486262263
Artem Pop <vip.artem.popov.14@mail.ru>
2026-03-17 12:41:29 +0500

    feat(domain): implement AggregateRoot and update Entity with event support
    Entity become strong abs type with TId VO generic, and
    IEquatable<EntityTId> implementation.
    AggregateRoot - new abs class that implementing Entity with TId VO
    generic, also gets opportunity to raise Domain events (Raise and Clear
    methods)

---
+++ src/LegacyLego.Domain/Shared/AggregateRoot.cs
@@ -0,0 +1,24 @@
+﻿namespace LegacyLego.Domain.Shared;
+
+public abstract class AggregateRoot<TId> : Entity<TId>
+    where TId : ValueObject
+{
+    private readonly List<IDomainEvent> _domainEvents = new();
+
+    public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+    protected AggregateRoot(TId id) : base(id)
+    {
+
+    }
+
+    protected void Raise(IDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
+}
--- src/LegacyLego.Domain/Shared/Entity.cs
+++ src/LegacyLego.Domain/Shared/Entity.cs
@@ -1,11 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using LegacyLego.Domain.Aggregates;
 
-namespace LegacyLego.Domain.Shared
+namespace LegacyLego.Domain.Shared;
+
+public abstract class Entity<TId> : IEquatable<Entity<TId>> 
+    where TId : ValueObject
 {
-    internal class Entity
-    {
 
+    public TId Id { get; init; }
+
+    protected Entity(TId id)
+    {
+        Id = id;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Entity<TId> entity && Equals(entity);
+    }
+
+    public bool Equals(Entity<TId>? other)
+    {
+        if (other is null || other.GetType() != GetType())
+            return false;
+
+        return Id.Equals(other.Id);
+
+    }
+
+    public static bool operator ==(Entity<TId>? left, Entity<TId>? right)
+    {
+        if (left is null ^ right is null) return false;
+        return left is null || left.Equals(right);
+    }
+
+    public static bool operator !=(Entity<TId>? left, Entity<TId>? right) => !(left == right);
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
     }
 }
