3f7a386911aba173f0aef4893b6ab4241ef0493c
Artem Pop <vip.artem.popov.14@mail.ru>
2026-04-28 14:12:39 +0500

    feature(application): add IPaymentProvider with Errors

---
+++ src/LegacyLego.Application/Abstractions/ExternalServices/IPaymentProvider.cs
@@ -0,0 +1,17 @@
+﻿using LegacyLego.Application.Payments.Common;
+using LegacyLego.Domain.Shared;
+
+namespace LegacyLego.Application.Abstractions.ExternalServices;
+
+public interface IPaymentProvider
+{
+    public Task<Result<PaymentSession>> CreatePaymentSessionAsync(
+        Guid paymentId,
+        decimal amount,
+        string currency,
+        CancellationToken ct);
+
+    public Task<Result<PaymentSession>> GetExistingSessionAsync(
+        Guid paymentId,
+        CancellationToken ct);
+}
---
+++ src/LegacyLego.Application/Errors/PaymentProviderErrors.cs
@@ -0,0 +1,18 @@
+﻿using LegacyLego.Domain.Enums;
+using LegacyLego.Domain.Shared;
+using LegacyLego.Domain.ValueObjects;
+
+namespace LegacyLego.Domain.Errors;
+
+public static class PaymentProviderErrors
+{
+    public const string SessionNotFoundCode = "PaymentProvider.SessionNotFoundByPaymentId";
+
+    public static Error GetSessionNotFoundByPaymentIdError(Guid paymentId)
+    {
+        return new(
+            Code: SessionNotFoundCode,
+            Message: $"По следующему OrderPayment Id: {paymentId} не было найдено ни одной активной сессии оплаты");
+    }
+
+}
