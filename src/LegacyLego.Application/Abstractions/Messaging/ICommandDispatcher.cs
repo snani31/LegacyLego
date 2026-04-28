86a5aa525af7c86671e102f56bb81f3e711878d6
Artem Pop <vip.artem.popov.14@mail.ru>
2026-04-28 14:59:13 +0500

    feat(application): add ICommandDispatcher & IEventPublisher abstractions

---
+++ src/LegacyLego.Application/Abstractions/Messaging/ICommandDispatcher.cs
@@ -0,0 +1,13 @@
+﻿using LegacyLego.Application.Abstractions.Messaging.Command;
+using LegacyLego.Domain.Shared;
+
+namespace LegacyLego.Application.Abstractions.Messaging;
+
+public interface ICommandDispatcher
+{
+    public Task<Result<TResult>> DispatchAsync<TCommand, TResult>(TCommand command, CancellationToken ct = default)
+        where TCommand : ICommand<TResult>;
+
+    public Task<Result> DispatchAsync<TCommand>(TCommand command, CancellationToken ct = default)
+        where TCommand : ICommand;
+}
---
+++ src/LegacyLego.Application/Abstractions/Messaging/IEventPublisher.cs
@@ -0,0 +1,9 @@
+﻿using LegacyLego.Domain.Shared;
+
+namespace LegacyLego.Application.Abstractions.Messaging;
+
+public interface IEventPublisher
+{
+    public Task PublishAsync<TEvent>(TEvent domainEvent, CancellationToken ct = default)
+        where TEvent : IDomainEvent;
+}
