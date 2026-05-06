de11d103740bc25a0f4d55999a021797b0d63829
Artem Pop <vip.artem.popov.14@mail.ru>
2026-05-06 19:09:38 +0500

    feature(specification): add specification pattern with IOrderRepository

--- src/LegacyLego.Domain/Abstractions/IOrderRepository.cs
+++ src/LegacyLego.Domain/Abstractions/IOrderRepository.cs
@@ -1,4 +1,5 @@
 ﻿using LegacyLego.Domain.Aggregates;
+using LegacyLego.Domain.Shared;
 using LegacyLego.Domain.ValueObjects;
 
 namespace LegacyLego.Domain.Abstractions;
@@ -8,6 +9,12 @@ public interface IOrderRepository
     public Task<Order?> GetByIdAsync(OrderId id, CancellationToken cancellationToken = default);
 
     public Task<IReadOnlyList<Order>> GetByClientIdAsync(Guid clientId, CancellationToken cancellationToken = default);
+
+    public Task<IReadOnlyList<TResult>> GetOrders<TResult>(Specification<Order,OrderId, TResult> specification, CancellationToken cancellationToken = default);
+
+    public Task<TResult?> GetOrder<TResult>(Specification<Order, OrderId, TResult> specification, CancellationToken cancellationToken = default);
+
+    public Task<int> GetOrdersCount(Specification<Order, OrderId> specification, CancellationToken cancellationToken = default);
 
     public void Add(Order order);
 }
---
+++ src/LegacyLego.Domain/Shared/Specification.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq.Expressions;
+
+namespace LegacyLego.Domain.Shared;
+
+public abstract class Specification<TEntity, TId, TResult> : Specification<TEntity, TId>
+    where TEntity : Entity<TId>
+    where TId : ValueObject
+{
+    public Expression<Func<TEntity, TResult>> Selector { get; }
+
+    protected Specification(
+        Expression<Func<TEntity, TResult>> selector)
+    {
+        Selector = selector;
+    }
+}
+
+public abstract class Specification<TEntity, TId>
+    where TEntity : Entity<TId>
+    where TId : ValueObject
+{
+    public int? SkipNum { get; private set; }
+
+    public int? LimitNum { get; private set; }
+
+    public List<Expression<Func<TEntity, bool>>> FilterExpressions { get; } = new();
+
+    public List<Expression<Func<TEntity, object>>> IncludeExpressions { get; } = new();
+
+    public List<Expression<Func<TEntity, object>>> OrderByExpressions { get; } = new();
+
+    public List<Expression<Func<TEntity, object>>> OrderByDescendingExpressions { get; } = new();
+
+    protected Specification() { }
+
+    protected void AddFilter(Expression<Func<TEntity, bool>> filterExpression) =>
+        FilterExpressions.Add(filterExpression);
+
+    protected void AddInclude(Expression<Func<TEntity, object>> includeExpression) =>
+        IncludeExpressions.Add(includeExpression);
+
+    protected void AddOrderBy(Expression<Func<TEntity, object>> orderByExpression) =>
+        OrderByExpressions.Add(orderByExpression);
+
+    protected void AddOrderByDescending(Expression<Func<TEntity, object>> orderByDescendingExpression) =>
+        OrderByDescendingExpressions.Add(orderByDescendingExpression);
+
+    protected void SetSkipNum(int skipNum) =>
+        SkipNum = skipNum;
+
+    protected void SetLimitNum(int limitNum) =>
+        LimitNum = limitNum;
+
+    protected void DropSkip() =>
+        SkipNum = null;
+
+    protected void DropLimit() =>
+        LimitNum = null;
+}
