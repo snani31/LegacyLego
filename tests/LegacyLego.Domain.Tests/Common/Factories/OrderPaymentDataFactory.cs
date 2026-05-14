e84feece7c8070e9fd572a78227075d9bc110030
Artem Pop <vip.artem.popov.14@mail.ru>
2026-05-14 14:30:04 +0500

    test(order-payment): add OrderPaymentDataFactory in Domain.Tests proj
    1) add OrderPaymentDataFactory
    2) static using OrderPaymentDataFactory in GlobalUsings.cs
    3) Microsoft.Extensions.TimeProvider.Testing v10.5.0 PackageReference

---
+++ tests/LegacyLego.Domain.Tests/Common/Factories/OrderPaymentDataFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace LegacyLego.Domain.Tests.Common.Factories;
+
+internal static class OrderPaymentDataFactory
+{
+    public static OrderPayment CreateDefaultOrderPayment()
+    {
+        return OrderPayment.Create(OrderId.New(), DateTime.UtcNow).Value;
+    }
+}
--- tests/LegacyLego.Domain.Tests/GlobalUsings.cs
+++ tests/LegacyLego.Domain.Tests/GlobalUsings.cs
@@ -1,8 +1,4 @@
-﻿global using TUnit.Core;
-global using TUnit.Assertions;
-global using TUnit.Assertions.Extensions;
-
-global using LegacyLego.Domain.Aggregates;
+﻿global using LegacyLego.Domain.Aggregates;
 global using LegacyLego.Domain.DomainEvents;
 global using LegacyLego.Domain.Enums;
 global using LegacyLego.Domain.Errors;
@@ -13,3 +9,4 @@ global using LegacyLego.Domain.ValueObjects;
 global using LegacyLego.Domain.Tests.Common.Builders;
 
 global using static LegacyLego.Domain.Tests.Common.Factories.OrderDataFactory;
+global using static LegacyLego.Domain.Tests.Common.Factories.OrderPaymentDataFactory;
