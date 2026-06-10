581bb6778d12343e8019012525cc619c8cde1089
Artem Pop <vip.artem.popov.14@mail.ru>
2026-06-10 21:23:47 +0500

    feature(infrastructure): Add OutboxBackgroundWorker &MockPaymentProvider

---
+++ src/LegacyLego.Infrastructure/BackgroundJobs/OutboxBackgroundWorker.cs
@@ -0,0 +1,110 @@
+﻿using LegacyLego.Application.Abstractions.Messaging;
+using LegacyLego.Domain.Shared;
+using LegacyLego.Infrastructure.Context;
+using LegacyLego.Infrastructure.Outbox;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
+namespace LegacyLego.Infrastructure.BackgroundJobs;
+
+public sealed class OutboxBackgroundWorker : BackgroundService
+{
+    private readonly IServiceProvider _serviceProvider;
+    private readonly ILogger<OutboxBackgroundWorker> _logger;
+    private readonly TimeSpan _period;
+
+    private readonly int _takeRecordsNum;
+
+    public OutboxBackgroundWorker(
+        IServiceProvider serviceProvider,
+        ILogger<OutboxBackgroundWorker> logger, 
+        int secondsPeriod = 2, 
+        int takeRecordsNum = 20)
+    {
+        _serviceProvider = serviceProvider;
+        _logger = logger;
+        _takeRecordsNum = takeRecordsNum;
+
+        _period = TimeSpan.FromSeconds(secondsPeriod);
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        _logger.LogInformation("Outbox Background Worker успешно запущен.");
+
+        using var timer = new PeriodicTimer(_period);
+
+        while (await timer.WaitForNextTickAsync(stoppingToken) && !stoppingToken.IsCancellationRequested)
+        {
+            try
+            {
+                await ProcessOutboxMessagesAsync(stoppingToken);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Критическая ошибка при выполнении фоновой задачи Outbox.");
+            }
+        }
+    }
+
+    private async Task ProcessOutboxMessagesAsync(CancellationToken ct)
+    {
+        using var scope = _serviceProvider.CreateScope();
+
+        var context = scope.ServiceProvider.GetRequiredService<OrderContext>();
+        var domainEventDispatcher = scope.ServiceProvider.GetRequiredService<IDomainEventDispatcher>();
+        var timeProvider = scope.ServiceProvider.GetRequiredService<TimeProvider>();
+
+        var messages = context.Set<OutboxMessage>()
+            .Where(m => m.ProcessedOnUtc == null)
+            .OrderBy(m => m.OccurredOnUtc)
+            .Take(_takeRecordsNum)
+            .ToList();
+
+        if (messages.Count == 0) return;
+
+        _logger.LogInformation("Найдено {Count} необработанных сообщений в Outbox.", messages.Count);
+
+        foreach (var message in messages)
+        {
+            try
+            {
+                // 1. Восстанавливаем .NET тип из строки AssemblyQualifiedName
+                Type? eventType = Type.GetType(message.Type);
+                if (eventType == null)
+                {
+                    _logger.LogError("Не удалось восстановить тип события: {Type}", message.Type);
+                    message.Error = $"Тип .NET '{message.Type}' не найден в сборках.";
+                    message.ProcessedOnUtc = timeProvider.GetUtcNow().UtcDateTime;
+                    continue;
+                }
+
+                // 2. Десериализуем JSON контент обратно в объект доменного события
+                var domainEvent = JsonSerializer.Deserialize(message.Content, eventType);
+                if (domainEvent is not IDomainEvent validEvent)
+                {
+                    _logger.LogError("Объект сообщения не реализует IDomainEvent");
+                    message.Error = "Объект десериализации не является IDomainEvent.";
+                    message.ProcessedOnUtc = timeProvider.GetUtcNow().UtcDateTime;
+                    continue;
+                }
+
+                await domainEventDispatcher.DispatchAsync(validEvent, ct);
+
+                // в случае успеха маркируем дату успешной обработки
+                message.ProcessedOnUtc = timeProvider.GetUtcNow().UtcDateTime;
+                message.Error = null; // очистить ошибки, если были ранее
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Ошибка при обработке Outbox сообщения с Id: {Id}", message.Id);
+
+                message.Error = ex.ToString();
+            }
+        }
+
+        await context.SaveChangesAsync(ct);
+    }
+}
---
+++ src/LegacyLego.Infrastructure/Services/MockPaymentProvider.cs
@@ -0,0 +1,100 @@
+﻿using LegacyLego.Application.Abstractions.ExternalServices;
+using LegacyLego.Application.Payments.Common;
+using LegacyLego.Domain.Shared;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Net.Http.Json;
+
+namespace LegacyLego.Infrastructure.Services;
+
+public sealed class MockPaymentProvider : IPaymentProvider
+{
+    private const string HTTP_ROUTE = "/mock/api/webhooks/payment";
+
+    private readonly HttpClient _httpClient;
+    private readonly TimeProvider _timeProvider;
+    private readonly string _baseUrl;
+    private readonly int _expiresAtMinutes;
+
+    public MockPaymentProvider(
+        HttpClient httpClient,
+        TimeProvider timeProvider,
+        string baseUrl,
+        int expiresAtMinutes)
+    {
+        _httpClient = httpClient;
+        _timeProvider = timeProvider;
+        _baseUrl = baseUrl;
+        _expiresAtMinutes = expiresAtMinutes;
+    }
+
+    public async Task<Result<PaymentSession>> CreatePaymentSessionAsync(
+        Guid paymentId,
+        Guid orderId,
+        decimal amount,
+        string currency,
+        CancellationToken ct)
+    {
+        string externalSessionId = GenerateExternalSession();
+
+        var expiresAtUtc = _timeProvider.GetUtcNow().AddMinutes(_expiresAtMinutes).UtcDateTime;
+
+        var queryParams = new Dictionary<string, string?>
+        {
+            { "paymentId", paymentId.ToString() },
+            { "orderId", orderId.ToString() },
+            { "amount", amount.ToString("F2") },
+            { "currency", currency },
+            { "externalSessionId", externalSessionId }
+        };
+
+        string checkoutUrl = QueryHelpers.AddQueryString(_baseUrl, queryParams);
+
+        var session = new PaymentSession(
+            PaymentId: paymentId,
+            ExternalSessionId: externalSessionId,
+            CheckoutUrl: checkoutUrl,
+            ExpiresAtUtc: expiresAtUtc
+        );
+
+        return Result<PaymentSession>.Success(session);
+    }
+
+    public async Task<Result> RequestRefundAsync(
+        Guid orderId,
+        decimal amount,
+        string currency,
+        string transactionId,
+        CancellationToken ct)
+    {
+        await Task.Delay(500, ct);
+
+        var payload = new ExternalStripeWebhookSimulation(
+            OrderId: orderId,
+            Amount: amount,
+            Currency: currency,
+            TransactionId: transactionId,
+            Status: "refund");
+
+        var response = await _httpClient.PostAsJsonAsync(HTTP_ROUTE, payload, ct);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            return Result.Failure(new Error(
+                "MockPayment.RefundFailed",
+                $"Имитация вебхука возврата завершилась ошибкой: {response.StatusCode}"));
+        }
+
+        return Result.Success();
+    }
+
+    private string GenerateExternalSession() =>  $"ext_sess_{Guid.NewGuid():N}";
+
+
+}
+
+file sealed record ExternalStripeWebhookSimulation(
+        Guid OrderId,
+        decimal Amount,
+        string Currency,
+        string TransactionId,
+        string Status);
