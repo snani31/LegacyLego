893883d5e4dda2e20f8b1c77ea31d0162b0e4095
Artem Pop <vip.artem.popov.14@mail.ru>
2026-06-19 18:01:06 +0500

    feature(dependency-injection): add modular DI to Inf & App projects
    1) DependencyInjection to Application & Infrastructure
    2) Add Scrutor v7.0.0 to LegacyLego.Application.csproj
    
    Application DependencyInjection include
    1) AddClasses ICommandHandler<> & ICommandHandler<,>
    2) AddClasses IQueryHandler<,>
    3) AddClasses IDomainEventHandler<>
    4) AddScoped PaymentLookup
    5) AddSingleton DomainExceptionMapper as IExceptionMapper
    
    Infrastructure DependencyInjection include:
    1) AddOptions DatabaseOptions
    2) AddOptions OutboxBackgroundWorkerOptions
    3) AddClasses IIntegrationEventConsumer<>
    4) AddDbContext OrderContext
    5) AddScoped UnitOfWork as IUnitOfWork
    6) AddScoped OrderRepository as IOrderRepository
    7) AddScoped PaymentRepository as IPaymentRepository
    8) AddSingleton InMemoryIntegrationEventBus as IIntegrationEventBus
    9) AddScoped LocalIntegrationEventPublisher as IIntegrationEventPublisher
    10) AddScoped QueryDispatcher as IQueryDispatcher
    11) AddScoped DomainEventDispatcher as IDomainEventDispatcher
    12) AddScoped CommandDispatcher as ICommandDispatcher
    13) AddSingleton InfrastructureExceptionMapper as IExceptionMapper
    14) AddScoped MockPaymentProvider as IPaymentProvider
    15) AddSingleton TimeProvider.System
    16) AddHostedService OutboxBackgroundWorker

---
+++ src/LegacyLego.Application/DependencyInjection.cs
@@ -0,0 +1,41 @@
+﻿using LegacyLego.Application.Abstractions.ExceptionHandling;
+using LegacyLego.Application.Abstractions.Messaging.Command;
+using LegacyLego.Application.Abstractions.Messaging.Event.Domain;
+using LegacyLego.Application.Abstractions.Messaging.Query;
+using LegacyLego.Application.Diagnostics;
+using LegacyLego.Application.Payments.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LegacyLego.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.Scan(scan => scan
+            .FromAssemblies(typeof(DependencyInjection).Assembly)
+
+            .AddClasses(classes => classes.AssignableTo(typeof(ICommandHandler<>)))
+                .AsImplementedInterfaces()
+                .WithScopedLifetime()
+
+            .AddClasses(classes => classes.AssignableTo(typeof(ICommandHandler<,>)))
+                .AsImplementedInterfaces()
+                .WithScopedLifetime()
+
+            .AddClasses(classes => classes.AssignableTo(typeof(IQueryHandler<,>)))
+                .AsImplementedInterfaces()
+                .WithScopedLifetime()
+
+            .AddClasses(classes => classes.AssignableTo(typeof(IDomainEventHandler<>)))
+                .AsImplementedInterfaces()
+                .WithScopedLifetime()
+        )
+            
+            .AddScoped<PaymentLookup>()
+            
+            .AddSingleton<IExceptionMapper, DomainExceptionMapper>();
+
+        return services;
+    }
+}
---
+++ src/LegacyLego.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,91 @@
+﻿using LegacyLego.Application.Abstractions.Data;
+using LegacyLego.Application.Abstractions.ExceptionHandling;
+using LegacyLego.Application.Abstractions.ExternalServices;
+using LegacyLego.Application.Abstractions.Messaging;
+using LegacyLego.Application.Abstractions.Messaging.Event.Integration;
+using LegacyLego.Application.Diagnostics;
+using LegacyLego.Domain.Abstractions;
+using LegacyLego.Infrastructure.BackgroundJobs;
+using LegacyLego.Infrastructure.Context;
+using LegacyLego.Infrastructure.Diagnostics;
+using LegacyLego.Infrastructure.Messaging.Abstractions;
+using LegacyLego.Infrastructure.Messaging.Bus;
+using LegacyLego.Infrastructure.Messaging.Dispatchers;
+using LegacyLego.Infrastructure.Messaging.Publishers;
+using LegacyLego.Infrastructure.Options;
+using LegacyLego.Infrastructure.Repositories;
+using LegacyLego.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace LegacyLego.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddOptions<DatabaseOptions>()
+            .BindConfiguration(DatabaseOptions.SectionName)
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
+
+        services.AddOptions<OutboxBackgroundWorkerOptions>()
+            .BindConfiguration(OutboxBackgroundWorkerOptions.SectionName)
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
+
+        services.Scan(scan => scan
+            .FromAssemblies(typeof(DependencyInjection).Assembly)
+
+            .AddClasses(classes => classes.AssignableTo(typeof(IIntegrationEventConsumer<>)))
+                .AsImplementedInterfaces()
+                .WithScopedLifetime())
+
+            .AddDbContext<OrderContext>((serviceProvider, options) =>
+            {
+                var dbOptions = serviceProvider.GetRequiredService<IOptions<DatabaseOptions>>().Value;
+                options.UseNpgsql(dbOptions.ConnectionString, npgsqlOptions =>
+                {
+                    npgsqlOptions.CommandTimeout(dbOptions.CommandTimeoutSeconds);
+
+                    npgsqlOptions.EnableRetryOnFailure(
+                        maxRetryCount: dbOptions.MaxRetryCount,
+                        maxRetryDelay: TimeSpan.FromSeconds(dbOptions.MaxRetryDelaySeconds),
+                        errorCodesToAdd: null);
+                });
+
+                if (dbOptions.EnableSensitiveDataLogging)
+                {
+                    options.EnableSensitiveDataLogging();
+                }
+
+                if (dbOptions.EnableDetailedErrors)
+                {
+                    options.EnableDetailedErrors();
+                }
+            })
+
+            .AddScoped<IUnitOfWork, UnitOfWork>()
+
+            .AddScoped<IOrderRepository, OrderRepository>()
+            .AddScoped<IPaymentRepository, PaymentRepository>()
+
+            .AddSingleton<IIntegrationEventBus, InMemoryIntegrationEventBus>()
+            .AddScoped<IIntegrationEventPublisher, LocalIntegrationEventPublisher>()
+
+            .AddScoped<ICommandDispatcher, CommandDispatcher>()
+            .AddScoped<IDomainEventDispatcher, DomainEventDispatcher>()
+            .AddScoped<IQueryDispatcher, QueryDispatcher>()
+
+            .AddSingleton<IExceptionMapper, InfrastructureExceptionMapper>()
+
+            .AddScoped<IPaymentProvider, MockPaymentProvider>()
+            .AddSingleton(TimeProvider.System)
+
+            .AddHostedService<OutboxBackgroundWorker>();
+
+        return services;
+    }
+}
