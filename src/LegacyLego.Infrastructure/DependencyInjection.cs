3729bfc744f5eb116e52733ad2d7e88bfb1d1d1e
Artem Pop <vip.artem.popov.14@mail.ru>
2026-06-26 15:56:55 +0500

    feature(infrastructure): add DependencyInjection new registrations
    1) Hangfire bacground services
    2) MockPaimentProvider with HttpClient
    3) Some new Options

--- src/LegacyLego.Infrastructure/DependencyInjection.cs
+++ src/LegacyLego.Infrastructure/DependencyInjection.cs
@@ -1,9 +1,11 @@
-﻿using LegacyLego.Application.Abstractions.Data;
+﻿using Hangfire;
+using Hangfire.PostgreSql;
+using LegacyLego.Application.Abstractions.Data;
 using LegacyLego.Application.Abstractions.ExceptionHandling;
 using LegacyLego.Application.Abstractions.ExternalServices;
 using LegacyLego.Application.Abstractions.Messaging;
+using LegacyLego.Application.Abstractions.Messaging.Command;
 using LegacyLego.Application.Abstractions.Messaging.Event.Integration;
-using LegacyLego.Application.Diagnostics;
 using LegacyLego.Domain.Abstractions;
 using LegacyLego.Infrastructure.BackgroundJobs;
 using LegacyLego.Infrastructure.Context;
@@ -26,7 +28,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddOptions<DatabaseOptions>()
+        var options = services.AddOptions<DatabaseOptions>()
             .BindConfiguration(DatabaseOptions.SectionName)
             .ValidateDataAnnotations()
             .ValidateOnStart();
@@ -36,12 +38,46 @@ public static class DependencyInjection
             .ValidateDataAnnotations()
             .ValidateOnStart();
 
+        services.AddOptions<PaymentProviderOptions>()
+            .BindConfiguration(PaymentProviderOptions.SectionName)
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
+
+        services.AddOptions<HangfireOptions>()
+            .BindConfiguration(HangfireOptions.SectionName)
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
+
+        var databaseOptions = configuration.GetSection(DatabaseOptions.SectionName).Get<DatabaseOptions>()
+                              ?? new DatabaseOptions();
+
+        var hangfireOptions = configuration.GetSection(HangfireOptions.SectionName).Get<HangfireOptions>()
+                              ?? new HangfireOptions();
+
+        var hangfirePostgreSqlStorageOptions = new PostgreSqlStorageOptions();
+        hangfirePostgreSqlStorageOptions.QueuePollInterval = TimeSpan.FromSeconds(hangfireOptions.QueuePollInterval);
+        hangfirePostgreSqlStorageOptions.SchemaName = "internal_jobs";
+
         services.Scan(scan => scan
             .FromAssemblies(typeof(DependencyInjection).Assembly)
 
             .AddClasses(classes => classes.AssignableTo(typeof(IIntegrationEventConsumer<>)))
                 .AsImplementedInterfaces()
                 .WithScopedLifetime())
+            
+            .AddHangfire(config => config
+            .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+            .UseSimpleAssemblyNameTypeSerializer()
+            .UseRecommendedSerializerSettings()
+            .UsePostgreSqlStorage(options => 
+            {
+                options.UseNpgsqlConnection(databaseOptions.ConnectionString);
+            }, hangfirePostgreSqlStorageOptions))
+            .AddHangfireServer(options =>
+            {
+                options.WorkerCount = hangfireOptions.WorkerCount;
+                options.Queues = new[] { HangfireOptions.CommandHangfireQueueName, "default" };
+            })
 
             .AddDbContext<OrderContext>((serviceProvider, options) =>
             {
@@ -81,10 +117,20 @@ public static class DependencyInjection
 
             .AddSingleton<IExceptionMapper, InfrastructureExceptionMapper>()
 
-            .AddScoped<IPaymentProvider, MockPaymentProvider>()
             .AddSingleton(TimeProvider.System)
 
+            .AddScoped<ICommandBackgroundJobService, HangfireCommandBackgroundJobService>()
+
             .AddHostedService<OutboxBackgroundWorker>();
+
+        services.AddHttpClient<IPaymentProvider, MockPaymentProvider>((serviceProvider, client) =>
+        {
+            var options = serviceProvider.GetRequiredService<IOptions<PaymentProviderOptions>>().Value;
+
+            //базовый адрес для ВСЕХ запросов этого клиента
+            client.BaseAddress = new Uri(options.ApiBaseUrl);
+        });
+
 
         return services;
     }
