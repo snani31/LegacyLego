ac052fdb199564b3a7ce2351e7959fec2dcbe824
Artem Pop <vip.artem.popov.14@mail.ru>
2026-06-26 16:07:19 +0500

    feature(presentation): Add OpenApiExtensions with Scalar graph
    1) Add OpenApiExtensions & ApiMetadataTransformer for simpler mapping
    2) Add new package reference Scalar.AspNetCore v2.16.4

---
+++ src/LegacyLego.Presentation/OpenApi/ApiMetadataTransformer.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi;
+
+namespace LegacyLego.Presentation.OpenApi;
+
+internal sealed class ApiMetadataTransformer : IOpenApiDocumentTransformer
+{
+    public Task TransformAsync(
+        OpenApiDocument document,
+        OpenApiDocumentTransformerContext context,
+        CancellationToken cancellationToken)
+    {
+        document.Info.Title = "LegacyLego E-Commerce API";
+        document.Info.Description = "Внутреннее API интернет-магазина конструкторов Lego. " +
+                                     "Обеспечивает работу с заказами, корзиной и платежными шлюзами.";
+
+        // ПРАВИЛЬНЫЙ ВАРИАНТ: Берем имя документа из контекста .NET OpenAPI!
+        // Если зарегистрирован документ "v1", то версия будет "v1"
+        document.Info.Version = $"openapi.{context.DocumentName}";
+
+        return Task.CompletedTask;
+    }
+}
---
+++ src/LegacyLego.Presentation/OpenApi/OpenApiExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace LegacyLego.Presentation.OpenApi;
+
+public static class OpenApiExtensions
+{
+    public const string ApiVersion = "v1";
+
+    public static IServiceCollection AddPresentationOpenApi(this IServiceCollection services)
+    {
+        return services.AddOpenApi(ApiVersion, options =>
+        {
+            options.AddDocumentTransformer<ApiMetadataTransformer>();
+        });
+    }
+}
