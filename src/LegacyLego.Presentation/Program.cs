d36e05e57aaa3c6f3997c41ca22e6c081d3fbca0
Artem Pop <vip.artem.popov.14@mail.ru>
2026-06-26 16:11:05 +0500

    feature(presentation): add new mappings in Program.cs
    1) Add wwwroot filder with mock-checkout.html for Payment lifecycle testing
    2) Set new options in appsettings.json for infrastructure services
    3) Map global exception handling to pipeline
    4) Add user secrets

--- src/LegacyLego.Presentation/Program.cs
+++ src/LegacyLego.Presentation/Program.cs
@@ -1,10 +1,45 @@
+using LegacyLego.Application;
+using LegacyLego.Application.Abstractions.ExternalServices;
+using LegacyLego.Infrastructure;
+using LegacyLego.Infrastructure.Services;
+using LegacyLego.Presentation.Middleware;
+using LegacyLego.Presentation.OpenApi;
 using LegacyLego.Presentation.Orders;
 using LegacyLego.Presentation.Payments;
+using Scalar.AspNetCore;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+configuration.AddUserSecrets(Assembly.GetExecutingAssembly(), false);
+
+builder.Services.AddApplication()
+    .AddInfrastructure(configuration)
+    .AddPresentationOpenApi();
+
+builder.Services.AddExceptionHandler<DynamicGlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
+
 var app = builder.Build();
 
-app.UseOrdersEndpoints();
-app.UsePaymentEndpoints();
+app.UseExceptionHandler(); // стоит самый первый в пайплайне
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+    app.MapScalarApiReference("/docs/scalar", options =>
+    {
+        options.WithTitle("LegacyLego Documentation")
+            .WithTheme(ScalarTheme.DeepSpace)
+            .WithClassicLayout()
+            .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient);
+    });
+}
+
+app.UseStaticFiles();
+
+app.MapOrdersEndpoints();
+app.MapPaymentEndpoints();
 
 app.Run();
